b391a16dd9de5f4393734a4973c750dc836a6223
Joshua Doud <bigjj3393@yahoo.com>
2023-01-21 09:21:09 -0800

    Created components and imported/routed to App

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import {Route, Switch} from "react-router-dom";
-import Navbar from "./Navbar"
-import Home from "./Home"
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Shelters from "./Shelters";
+import Animals from "./Animals";
+import Form from "./Form"
 
 
 function App() {
@@ -15,9 +18,18 @@ function App() {
         <Route exact path="/">
           <Home/>
         </Route>
+        <Route exact path ="/Shelters">
+          <Shelters/>
+        </Route>
+        <Route exact path ="/Animals">
+          <Animals/>
+        </Route>
+        <Route exact path ="/Form">
+          <Form/>
+        </Route>
       </Switch>
       <div id="footer">
-        
+
       </div>
     </>
   );
