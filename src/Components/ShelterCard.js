4f8553d8b8be5e8bb6993daa1d3538b97b43f3fd
Joshua Doud <bigjj3393@yahoo.com>
2023-01-21 11:20:08 -0800

    Rendered Shelter Card to Dom using prop Shelters

---
+++ src/Components/ShelterCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ShelterCard({shelter}) {
+    return (
+        <div className="shelter-card">
+            <img className="shelterimg" src={shelter.image}></img>
+            <h3>{shelter.name}</h3>
+            <h4>Contact Information:</h4>
+            <h5>Phone Number: {shelter.phone}</h5>
+            <h5>Email Address: {shelter.email}</h5>
+        </div>
+    )
+}
+
+export default ShelterCard
--- src/Components/Shelters.js
+++ src/Components/Shelters.js
@@ -1,7 +1,16 @@
 import React from "react";
+import ShelterCard from "./ShelterCard"
 
-function Shelters() {
-    return(<h1>shelters</h1>)
+function Shelters({shelters}) {
+
+    const shelterList = shelters.map((shelter) => <ShelterCard key={shelter.id} shelter={shelter}/>)
+
+    return (
+        <div id="shelter-disp">
+            {shelterList}
+        </div>
+    )
+
 }
 
 export default Shelters
