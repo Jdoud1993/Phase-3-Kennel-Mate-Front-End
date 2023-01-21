4ded8cdacdec50376c169c84bce953f9cc828d84
Joshua Doud <bigjj3393@yahoo.com>
2023-01-21 10:18:05 -0800

    Experimented with bootstrap but deleted

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
+
 function Home() {
-    return(<h1>home</h1>)
+    return(
+        <div id="Home">
+            <h1>home</h1>
+        </div>
+    )
 }
 
 export default Home
